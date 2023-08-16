554df32ee9c4e7f3fab88c6fdb425fba7b518f5d
Robert Nurden <Rob.Nurden@iress.com>
2023-08-16 17:50:08 +0100

    test: start listing test cases

--- BankingApplicationTests/OptionsListTests.cs
+++ BankingApplicationTests/OptionsListTests.cs
@@ -1,11 +1,20 @@
 namespace BankingApplicationTests
 {
+    [TestFixture]
     public class Tests
     {
         [SetUp]
         public void Setup()
         {
         }
+
+        // DisplayOptionsTests
+        // When option is 1, deposit option is called
+        // When option is 1, display balance is called
+        // repeat for options 2, 3 and 4.
+
+        // SelectOptionTests
+        // When method called, returns a valid integer.
 
         [Test]
         public void Test1()
