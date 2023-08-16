41c7fb6b37fab40f5e977b8597e02cff0751ef4e
Robert Nurden <Rob.Nurden@iress.com>
2023-08-16 15:38:09 +0100

    feat: add deposit and display balance functions

--- Banking Application/Program.cs
+++ Banking Application/Program.cs
@@ -1,13 +1,24 @@
 ﻿using System.ComponentModel.Design;
+using Console = System.Console;
 
 namespace BankingApplication;
 class Program
 {
+    private static int _balance;
+
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to RoBank!");
         var option = Options();
-        Console.ReadLine();
+
+        if (option == 1)
+        {
+            Deposit();
+        }
+        if (option == 2)
+        {
+            DisplayBalance();
+        }
     }
 
     public static int Options()
@@ -37,5 +48,35 @@ class Program
         Console.WriteLine($"You have selected option {option}");
 
         return option;
+    }
+
+    public static void Deposit()
+    {
+        int amount;
+        var isValid = false;
+        do
+        {
+            Console.WriteLine("How much would you like to deposit? ");
+            var strAmount = Console.ReadLine();
+
+            if (int.TryParse(strAmount, out amount) && amount is < 1000000 and > 0)
+            {
+                isValid = true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, please enter a number between 1 and 1,000,000.");
+            }
+
+        } while (!isValid);
+
+        _balance =+ amount;
+
+        DisplayBalance();
+    }
+
+    public static void DisplayBalance()
+    {
+        Console.WriteLine($"Your current balance is {_balance}");
     }
 }
