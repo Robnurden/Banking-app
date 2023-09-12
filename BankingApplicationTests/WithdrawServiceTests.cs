08de0addc7f58fc4d4df24af9aceaf3afda859e7
Robert Nurden <Rob.Nurden@iress.com>
2023-09-12 15:42:16 +0100

    test: use step up method for tests

--- BankingApplicationTests/WithdrawServiceTests.cs
+++ BankingApplicationTests/WithdrawServiceTests.cs
@@ -7,20 +7,27 @@ namespace BankingApplicationTests
     [TestFixture]
     public class WithdrawServiceTests
     {
+        private WithdrawService _withdrawService;
+        private Mock<ConsoleWrapper> _consoleWrapper;
+
         private const decimal MinWithdrawal = 1;
         private const decimal BalanceThreshold = 0;
+
+        [SetUp]
+        public void Setup()
+        {
+            _consoleWrapper = new Mock<ConsoleWrapper>();
+            _withdrawService = new WithdrawService(_consoleWrapper.Object, BalanceThreshold);
+        }
 
         [TestCase("50", 100, 50)]
         [TestCase("50.02", 100.01, 49.99)]
         public void WithdrawOrchestrator_ReturnsBalance_WhenValidValueIsEntered(string? strAmount, decimal balance, decimal expectedResult)
         {
-            var consoleMock = new Mock<ConsoleWrapper>();
-            consoleMock.Setup(c => c.ReadLine())
+            _consoleWrapper.Setup(c => c.ReadLine())
                 .Returns(strAmount);
 
-            var withdrawService = new WithdrawService(consoleMock.Object, BalanceThreshold);
-
-            var result = withdrawService.WithdrawOrchestrator(balance);
+            var result = _withdrawService.WithdrawOrchestrator(balance);
 
             result.Should().Be(expectedResult);
         }
@@ -30,15 +37,14 @@ namespace BankingApplicationTests
         {
             const int balance = 100;
             var expectedString = $"\nInvalid input. Please enter an amount up to two decimal places, between {MinWithdrawal} and less than {balance + (decimal)0.01}.";
-            var consoleMock = new Mock<ConsoleWrapper>();
-            consoleMock.SetupSequence(c => c.ReadLine())
+
+            _consoleWrapper.SetupSequence(c => c.ReadLine())
                 .Returns(" ")
                 .Returns("1");
 
-            var withdrawService = new WithdrawService(consoleMock.Object, BalanceThreshold);
-            withdrawService.WithdrawOrchestrator(balance);
+            _withdrawService.WithdrawOrchestrator(balance);
 
-            consoleMock.Verify(c => c.WriteLine(expectedString));
+            _consoleWrapper.Verify(c => c.WriteLine(expectedString));
         }
 
         [Test]
@@ -46,13 +52,11 @@ namespace BankingApplicationTests
         {
             const int balance = 0;
             var expectedString = $"Current balance is {balance}. Please deposit funds before you can withdraw.";
-            var consoleMock = new Mock<ConsoleWrapper>();
 
-            var withdrawService = new WithdrawService(consoleMock.Object, BalanceThreshold);
-            var result = withdrawService.WithdrawOrchestrator(balance);
+            var result = _withdrawService.WithdrawOrchestrator(balance);
 
             result.Should().Be(balance);
-            consoleMock.Verify(c => c.WriteLine(expectedString));
+            _consoleWrapper.Verify(c => c.WriteLine(expectedString));
         }
 
         [TestCase("1", 2, true)]
@@ -66,8 +70,7 @@ namespace BankingApplicationTests
         [TestCase("hello", 100, false)]
         public void ValidateWithdrawalAmount_ShouldReturnExpectedValue(string? strAmount, decimal balance, bool expectedResult)
         {
-            var withdrawService = new WithdrawService(new ConsoleWrapper(), BalanceThreshold);
-            var result = withdrawService.ValidateWithdrawalAmount(strAmount, balance);
+            var result = _withdrawService.ValidateWithdrawalAmount(strAmount, balance);
 
             result.Should().Be(expectedResult);
         }
