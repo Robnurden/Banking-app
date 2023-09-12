9e1604ea56e27ddca4a6739e3f53d747f0df6d04
Robert Nurden <Rob.Nurden@iress.com>
2023-09-12 15:15:47 +0100

    test: add test for balance service

---
+++ BankingApplicationTests/BalanceServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Banking_Application;
+using Moq;
+
+namespace BankingApplicationTests
+{
+    [TestFixture]
+    public class BalanceServiceTests
+    {
+        [Test]
+        public void DisplayBalance_OutputsBalanceToConsole_WithCultureSymbol()
+        {
+            const decimal balance = (decimal)100.19;
+            var expectedResult = $"\nYour current balance is £{balance}";
+            var consoleMock = new Mock<ConsoleWrapper>();
+
+            var balanceService = new BalanceService(consoleMock.Object);
+
+            balanceService.DisplayBalance(balance);
+
+            consoleMock.Verify(c => c.WriteLine(expectedResult));
+        }
+    }
+}
